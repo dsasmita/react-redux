5a6669a1436d09184409732b1f4268989ebe181d
dsasmita <dangs.work@gmail.com>
2020-03-12 21:30:01 +0700

    implement redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { createStore } from "redux";
+
+// Reducer
+function reducer(state = 0, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    case "GET":
+      console.log(state);
+      return state;
+    default:
+      return state;
+  }
+}
+
+// Store
+const store = createStore(reducer);
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  // Subscribe
+  useEffect(() => {
+    store.subscribe(() => {
+      setCounter(store.getState());
+    });
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Counter</h1>
+      <h2>{counter}</h2>
+      <button
+        onClick={() => {
+          store.dispatch({ type: "INCREMENT" });
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          store.dispatch({ type: "DECREMENT" });
+        }}
+      >
+        -
+      </button>
+      <button
+        onClick={() => {
+          store.dispatch({ type: "GET" });
+        }}
+      >
+        =
+      </button>
+    </>
   );
 }
 
